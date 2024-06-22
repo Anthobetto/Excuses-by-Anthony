feb0bcf6a9eeffbe4776f1ead9f18498ae357481
Anthobetto <64795591+Anthobetto@users.noreply.github.com>
2024-06-22 12:06:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function AnExcuse(QWho, Qaction, Qwhat, Qwhen) {
+  QWho = who[Math.floor(Math.random() * who.length)];
+  Qaction = action[Math.floor(Math.random() * action.length)];
+  Qwhat = what[Math.floor(Math.random() * what.length)];
+  Qwhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return QWho + " " + Qaction + " " + Qwhat + " " + Qwhen;
+}
+
+function OnLoad() {
+  document.getElementById("excuse").innerHTML = AnExcuse();
+}
+
+window.onload = OnLoad();
